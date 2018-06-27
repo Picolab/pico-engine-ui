01ad7dcb10cc88ed27992f79e98a772c194342ea
cgrimm013 <cagrimm13@gmail.com>
2018-06-27 15:28:23 -0600

    Prevent removal of entry pico
    I added a query and check to prevent the removal of the entry pico from view.

--- src/sagas/removePicoFromView.js
+++ src/sagas/removePicoFromView.js
@@ -1,13 +1,25 @@
 import { call, put } from 'redux-saga/effects';
-import { removeSettingsEntry } from '../utils/picoSDK';
+import { removeSettingsEntry, getPicoId } from '../utils/picoSDK';
 import { getEntryDID, getEntryHost } from '../config';
 import { retrieveSettings } from '../actions';
 
 export default function* removePicoFromView(action) {
   if(action.payload && action.payload.DID && action.payload.host && action.payload.picoID) {
+    const picoID = action.payload.picoID;
     try {
-      yield call(removeSettingsEntry, getEntryDID(), getEntryHost(), action.payload.picoID);
-      yield put(retrieveSettings());
+      //see if the picoID is the entry pico id. If it is, don't remove it
+      const result = yield call(getPicoId, getEntryDID(), getEntryHost());
+      const entryPicoID = result.data
+      if(entryPicoID && typeof entryPicoID === "string"){
+        if(entryPicoID !== picoID) {
+          yield call(removeSettingsEntry, getEntryDID(), getEntryHost(), action.payload.picoID);
+          yield put(retrieveSettings());
+        }else {
+          alert("You cannot remove the entry pico from view!")
+        }
+      }else {
+        console.error("Malformed response in removePicoFromView! Expected entryPicoID to be a string, received:", entryPicoID);
+      }
     }catch(e) {
       console.error(e);
       if(e.response && e.response.data && e.response.data.error){
